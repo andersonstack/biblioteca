d878f0deb262534f15438ba4d94076b5207a320d
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-06-08 15:24:03 -0300

    feat: criando componente TextButton

---
+++ src/components/text_button.ts
@@ -0,0 +1,52 @@
+class TextButton extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});
+    }
+
+    connectedCallback(){
+        const page = this.getAttribute('href') || '#';
+        const classButton = this.getAttribute('class') || 'default';
+
+        this.shadowRoot!.innerHTML = `
+            <style>
+            .with_background,
+            .default {
+                font-weight: 800;
+                text-decoration: none;
+                border-radius: 1rem;
+            }
+
+            .with_background {
+                background-color: var(--black);
+                padding: 1rem;
+                width: 100%;
+                color: white;
+                transition: 0.5s ease-in-out;
+                
+                &:hover {
+                    background-color: var(--azul);
+                }
+
+            }
+
+            .default {
+                background-color: transparent;
+                text-decoration: none;
+                color: black;
+                transition: 0.5s ease-in-out;
+
+                &:hover {
+                    color: var(--azul);
+                }
+            }
+            </style>
+
+            <a href=${page} class=${classButton}>
+                <slot></slot>
+            </a>
+        `;
+    }
+}
+
+customElements.define('text-button', TextButton);
