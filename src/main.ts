59283cc90949cd5562a6c6f0f35de2b4b3dea929
Anderson Gabriel <andersong.pereiracruz@gmail.com>
2025-07-05 11:29:50 -0300

    chore: formatação

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import './components/text_button.js';
-import './components/input_button.js';
-import './components/input.js';
-import './components/button.js';
-import './components/nav.js';
-import './components/card.js';
+import "./components/text_button.js";
+import "./components/input_button.js";
+import "./components/input.js";
+import "./components/button.js";
+import "./components/nav.js";
+import "./components/card.js";
 
-import './interfaces/card_interface.js';
-import './scripts/carrousel.js';
+import "./interfaces/card_interface.js";
+import "./scripts/carrousel.js";
 
-import './scripts/categorias.js';
+import "./scripts/categorias.js";
